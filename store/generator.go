0de647c8875d2375b360303e2722bc0a3308e30c
heidi-ann <hh360@cam.ac.uk>
2016-02-09 17:08:17 +0000

    adding variable conflict

--- store/generator.go
+++ store/generator.go
@@ -1,12 +1,15 @@
 package store
 
 import (
+	"fmt"
 	"math/rand"
 )
 
+// Generator generates workloads for the store
+// Store has 10 keys
 type Generator struct {
 	Ratio    int // percentage of read requests
-	Conflict int // percentage of requests which target particular area
+	Conflict int // 1 to 5, degree of requests which target particular area
 }
 
 func Generate(ratio int, conflict int) Generator {
@@ -14,9 +17,24 @@ func Generate(ratio int, conflict int) Generator {
 }
 
 func (g Generator) Next() string {
-	if rand.Intn(100) < g.Ratio {
-		return "get A\n"
+
+	key := "A"
+
+	// determine which key to operate on
+	// range 0-9
+	if rand.Intn(5) < g.Conflict-1 {
+		// non-conflicted region
+		// range conflict to 9
+		key = string(9 - rand.Intn(10-g.Conflict))
 	} else {
-		return "update A B\n"
+		// conflicted region
+		// range 0 to (conflict-1)
+		key = string(rand.Intn(g.Conflict))
+	}
+
+	if rand.Intn(100) < g.Ratio {
+		return fmt.Sprintf("get %s\n", key)
+	} else {
+		return fmt.Sprintf("update %s 7\n", key)
 	}
 }
