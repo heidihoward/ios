26f6183b929f4d25e6f3bbb8df710d3e807773bf
heidi-ann <hh360@cam.ac.uk>
2016-03-22 10:02:04 +0000

    fixing cleanup in simple test script

--- scripts/simple_test.sh
+++ scripts/simple_test.sh
@@ -4,8 +4,8 @@
 # tidy up from previous tests
 cd $GOPATH/src/github.com/heidi-ann/hydra
 
-rm *.temp
-rm latency*.csv
+rm server/persistent_data_*.temp
+rm client/latency_*.csv
 
 # start server
 cd server
@@ -14,8 +14,8 @@ $GOPATH/bin/server -id=1 -client-port=8081 -peer-port=8091 &
 $GOPATH/bin/server -id=2 -client-port=8082 -peer-port=8092 &
 
 # start clients 
-cd client
-$GOPATH/bin/client -id=0  -mode=test -stat=latency_1.csv &
+cd ../client
+$GOPATH/bin/client -id=0 -mode=test -stat=latency_1.csv &
 $GOPATH/bin/client -id=1 -mode=test -stat=latency_2.csv &
 $GOPATH/bin/client -id=2 -mode=test -stat=latency_3.csv &
 
