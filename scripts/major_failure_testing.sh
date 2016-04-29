9bff4388d3cf01b161e85acd06ea5e7dff93358a
heidi-ann <hh360@cam.ac.uk>
2016-04-29 16:46:53 +0100

    readding node 0 recovery

--- scripts/major_failure_testing.sh
+++ scripts/major_failure_testing.sh
@@ -12,15 +12,15 @@ sleep 0.03
 echo "stopping node ID 0"
 kill $PID
 
-# # restart node ID 0
-# sleep 0.03
-# cd server
+# restart node ID 0
+sleep 0.03
+cd server
 
-# echo "restarting node ID 0"
-# $GOPATH/bin/server -id=0 -client-port=8080 -peer-port=8090 -config=../scripts/serv.conf -log_dir=../scripts/results/$TIME/3s1c/s2.log &
-# cd ..
+echo "restarting node ID 0"
+$GOPATH/bin/server -id=0 -client-port=8080 -peer-port=8090 -config=../scripts/serv.conf -log_dir=../scripts/results/$TIME/3s1c/s2.log &
+cd ..
 
-# # stop experiment
-# sleep 5
-# echo "ending experiment"
-# ./scripts/stop_system.sh
+# stop experiment
+sleep 5
+echo "ending experiment"
+./scripts/stop_system.sh
