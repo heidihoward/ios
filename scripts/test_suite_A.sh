026dfaa5bd9abc955a72df5ef02480e8e742fa8e
heidi-ann <hh360@cam.ac.uk>
2016-05-11 17:13:12 +0100

    new more complete benchmarking suite

---
+++ scripts/test_suite_A.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Run complete test suite
+
+TIME=`date '+%m-%d-%H%M%S'` 
+
+# start servers
+echo "Staring servers and clients"
+./scripts/start_system.sh 3 1 scripts/results/$TIME/simple
+
+# stop experiment
+sleep 5
+echo "ending experiment"
+./scripts/stop_system.sh
+
+# testing latency from 1 to 10 clients with a 3 server system
+for (( i = 1; i < 10; i++ )); do
+
+	# start
+	./scripts/start_system.sh 3 $i scripts/results/$TIME/load
+
+	# stop 
+	sleep 5
+	./scripts/stop_system.sh
+	sleep 1
+done
+
+
+
+# testing latency for 3 to 9 servers with a single client
+for (( i = 3; i <= 9; i++ )); do
+
+	# start
+	./scripts/start_system.sh $i 1 scripts/results/$TIME/scale
+
+	# stop 
+	sleep 5
+	./scripts/stop_system.sh
+	sleep 1
+done
