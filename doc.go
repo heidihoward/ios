f6850e7d8196a376eb54f069563a3bff5f7ee4dc
heidi-ann <hh360@cam.ac.uk>
2017-01-30 15:54:39 +0000

    fixing doc.go

--- doc.go
+++ doc.go
@@ -1,5 +1,6 @@
-Welcome to Ios, a distributed and strongly consistent key-value store, built upon a novel delegated and decentralised consensus protocol.
+// Welcome to Ios, a distributed and strongly consistent key-value store,
+// built upon a novel delegated and decentralised consensus protocol.
 
-This repository is pre-alpha and under active development. APIs will be broken.
+// This repository is pre-alpha and under active development. APIs will be broken.
 
 package ios
