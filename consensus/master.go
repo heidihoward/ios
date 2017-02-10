40ca7e67feda0913b8375c1d755e9d5d3fd7e851
heidi-ann <hh360@cam.ac.uk>
2017-02-10 17:26:49 +0000

    removing support for non-replciated reads

--- consensus/master.go
+++ consensus/master.go
@@ -59,123 +59,81 @@ func RunMaster(view int, commit_index int, initial bool, io *msgs.Io, config Con
 	}
 	window_start := index
 
-	if config.BatchInterval == 0 {
-		glog.Info("Ready to handle requests. No batching enabled")
-		// handle client requests (1 at a time)
-		for {
+	for {
+		glog.Info("Ready to handle request")
+		req1 := <-io.IncomingRequests
+		glog.Info("Request received: ", req1)
+		var reqs []msgs.ClientRequest
 
-			// wait for request
-			req := <-io.IncomingRequests
-			glog.Info("Request received: ", req)
-
-			// if possible, handle request without replication
-			if !req.Replicate {
-				io.OutgoingRequests <- req
-				glog.Info("Read-only request was handled without replication: ", req)
-			} else {
-
-				//wait for window slot
-				for index > window_start + config.WindowSize {
-					time.Sleep(100 * time.Millisecond)
-				}
-				index++
-				glog.Info("Request assigned index: ", index)
-
-				// ok := RunCoordinator(view, index, []msgs.ClientRequest{req}, io, config, true)
-				entry := msgs.Entry{view, false, []msgs.ClientRequest{req}}
-				coord := msgs.CoordinateRequest{config.ID, view, index, true, entry}
-				io.OutgoingUnicast[coordinator].Requests.Coordinate <- coord
-				// TODO: BUG: need to handle coordinator failure
-				go func() {
-					reply := <-(*io).Incoming.Responses.Coordinate
-					// TODO: check msg replies to the msg we just sent
-					if !reply.Response.Success {
-						glog.Warning("Commit unsuccessful")
-					}
-					glog.Info("Finished replicating request: ", req)
-					if reply.Request.Index==window_start+1{
-						window_start += 1
-					} else {
-						// TODO: BUG: handle out-of-order commitment
-						glog.Fatal("STUB: to implement")
-					}
-				}()
-
-				// rotate coordinator is nessacary
-				if config.DelegateReplication > 1 {
-					coordinator += 1
-					if coordinator>config.ID + config.DelegateReplication {
-						coordinator = config.ID + 1
-					}
-				}
-			}
-
+		//wait for window slot
+		//TOOD: replace with better mechanism then polling
+		for index > window_start + config.WindowSize {
+			time.Sleep(10 * time.Millisecond)
 		}
-	} else {
-		glog.Info("Ready to handle request. Batch every ", config.BatchInterval, " milliseconds")
-		for {
-			// setup for holding requests
-			reqs := make([]msgs.ClientRequest, config.MaxBatch)
-			reqs_num := 0
 
-			// start collecting requests
-			timeout := make(chan bool, 1)
-			go func() {
-				<-time.After(time.Millisecond * time.Duration(config.BatchInterval))
-				timeout <- true
-			}()
+		if config.BatchInterval == 0 || config.MaxBatch == 1 {
+			glog.Info("No batching enabled")
+			// handle client requests (1 at a time)
+			reqs = []msgs.ClientRequest{req1}
+		} else {
+			glog.Info("Ready to handle more requests. Batch every ", config.BatchInterval, " milliseconds")
+			// setup for holding requests
+			reqs_all := make([]msgs.ClientRequest, config.MaxBatch)
+			reqs_num := 1
+			reqs_all[0] = req1
 
 			exit := false
 			for exit == false {
 				select {
 				case req := <-io.IncomingRequests:
-					if !req.Replicate {
-						io.OutgoingRequests <- req
-						glog.Info("Request handled without replication: ", req)
-					} else {
-						reqs[reqs_num] = req
+						reqs_all[reqs_num] = req
 						glog.Info("Request ", reqs_num, " is : ", req)
 						reqs_num = reqs_num + 1
 						if reqs_num == config.MaxBatch {
 							exit = true
 							break
 						}
-					}
-				case <-timeout:
+				case <-time.After(time.Millisecond * time.Duration(config.BatchInterval)):
 					exit = true
 					break
 				}
 			}
-
-			// assign requests to coordinators
-			if reqs_num > 0 {
-				glog.Info("Starting to replicate ", reqs_num, " requests")
-				reqs_small := reqs[:reqs_num]
-				index++
-				// ok := RunCoordinator(view, index, reqs_small, io, config, true)
-				entry := msgs.Entry{view, false, reqs_small}
-				coord := msgs.CoordinateRequest{config.ID, view, index, true, entry}
-				io.OutgoingUnicast[coordinator].Requests.Coordinate <- coord
-				go func() {
-					reply := <- io.Incoming.Responses.Coordinate
-					// TODO: check msg replies to the msg we just sent
-					if !reply.Response.Success {
-						glog.Warning("Commit unsuccessful")
-					}
-					glog.Info("Finished replicating requests: ", reqs_small)
-				} ()
-				// rotate coordinator is nessacary
-				if config.DelegateReplication > 1 {
-					coordinator += 1
-					if coordinator>config.ID + config.DelegateReplication {
-						coordinator = config.ID + 1
-					}
-				}
-			}
+			// this batch is ready
+			glog.Info("Starting to replicate ", reqs_num, " requests")
+			reqs = reqs_all[:reqs_num]
 		}
 
-	}
+		index++
+		glog.Info("Request assigned index: ", index)
 
+		// ok := RunCoordinator(view, index, []msgs.ClientRequest{req}, io, config, true)
+		entry := msgs.Entry{view, false, reqs}
+		coord := msgs.CoordinateRequest{config.ID, view, index, true, entry}
+		io.OutgoingUnicast[coordinator].Requests.Coordinate <- coord
+		// TODO: BUG: need to handle coordinator failure
+		go func() {
+			reply := <-(*io).Incoming.Responses.Coordinate
+			// TODO: check msg replies to the msg we just sent
+			if !reply.Response.Success {
+				glog.Warning("Commit unsuccessful")
+			}
+			glog.Info("Finished replicating request: ", reqs)
+			if reply.Request.Index==window_start+1{
+				window_start += 1
+			} else {
+				// TODO: BUG: handle out-of-order commitment
+				glog.Fatal("STUB: to implement")
+			}
+		}()
+
+		// rotate coordinator is nessacary
+		if config.DelegateReplication > 1 {
+			coordinator += 1
+			if coordinator>config.ID + config.DelegateReplication {
+				coordinator = config.ID + 1
+			}
+		}
+	}
 	glog.Warning("Master stepping down")
 
 }
