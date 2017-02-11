d138d36c59de1f57efa5a0581f829d208778347f
Heidi-ann <hh360@cam.ac.uk>
2017-02-11 21:21:19 +0000

    adding validation to server config files

--- config/server.go
+++ config/server.go
@@ -24,5 +24,24 @@ func ParseServerConfig(filename string) ServerConfig {
 	if err != nil {
 		glog.Fatalf("Failed to parse gcfg data: %s", err)
 	}
+	// checking configuation is sensible
+	if len(config.Peers.Address) == 0 {
+		glog.Fatalf("At least one server is required")
+	}
+	if config.Options.Length <= 0 {
+		glog.Fatalf("Log length must be at least 1")
+	}
+	if config.Options.BatchInterval < 0 {
+		glog.Fatalf("Batch interval must be positive")
+	}
+	if config.Options.MaxBatch < 0 {
+		glog.Fatalf("Max batch size must be positive")
+	}
+	if config.Options.DelegateReplication < 0 || config.Options.DelegateReplication > len(config.Peers.Address) {
+		glog.Fatalf("Batch interval must be positive")
+	}
+	if config.Options.WindowSize <= 0 {
+		glog.Fatalf("Window Size must be greater than one")
+	}
 	return config
 }
