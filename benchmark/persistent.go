a1556ff880eb3b8c5d13b8f2d8860f5a92653e7f
heidi-ann <hh360@cam.ac.uk>
2017-03-10 14:15:03 +0000

    switching to recording total time

--- benchmark/persistent.go
+++ benchmark/persistent.go
@@ -8,15 +8,15 @@ import (
 )
 
 func benchmarkDisk(filename string, size int, count int) {
+	startTime := time.Now()
 	bytes := make([]byte, size)
   file, _ := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
   for i := 0; i < count; i++ {
 		rand.Read(bytes)
     file.Write(bytes)
-		startTime := time.Now()
     file.Sync()
-		fmt.Printf("%s\n",time.Since(startTime).String())
   }
+	fmt.Printf("%s\n",time.Since(startTime).String())
 }
 
 
