655e03ef922b081f82572c9140cfd0bc0f3efdd9
heidi-ann <hh360@cam.ac.uk>
2017-03-14 12:38:41 +0000

    fixing text alignment for interactive console

--- services/services.go
+++ services/services.go
@@ -21,20 +21,20 @@ func GetInteractiveText(config string) string {
 	var s string
 	switch config {
 	case "kv-store":
-		s = `
-	The following commands are available:
-		get [key]: to return the value of a given key
-		exists [key]: to test if a given key is present
-		update [key] [value]: to set the value of a given key, if key already exists then overwrite
-		delete [key]: to remove a key value pair if present
-		count: to return the number of keys
-		print: to return all key value pairs
-	`
+		s =
+`The following commands are available:
+	get [key]: to return the value of a given key
+	exists [key]: to test if a given key is present
+	update [key] [value]: to set the value of a given key, if key already exists then overwrite
+	delete [key]: to remove a key value pair if present
+	count: to return the number of keys
+	print: to return all key value pairs
+`
 	case "dummy":
-		s = `
-	The following commands are available:
+		s =
+`The following commands are available:
 		ping: ping dummy application
-	`
+`
 	}
 	return s
 }
