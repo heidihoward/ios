7454eb765c52cdf402e56e1c47b46044b02f2135
heidi-ann <hh360@cam.ac.uk>
2017-03-15 15:59:29 +0000

    Ios now building on darwin

---
+++ unix/wal_darwin.go
@@ -0,0 +1,51 @@
+// +build darwin
+
+package unix
+
+import (
+	"github.com/golang/glog"
+	"os"
+	"syscall"
+	"time"
+)
+
+type WAL struct {
+	fd   int
+	mode string
+}
+
+func openWriteAheadFile(filename string, mode string) WAL {
+	var file int
+	var err error
+	switch mode {
+	case "none", "fsync":
+		file, err = syscall.Open(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	default:
+		glog.Fatal("PersistenceMode not recognised, only fsync and none are avalible on darwin")
+	}
+	if err != nil {
+		glog.Fatal(err)
+	}
+	if err != nil {
+		glog.Fatal(err)
+	}
+	return WAL{file, mode}
+}
+
+func (w WAL) writeAhead(bytes []byte) {
+	startTime := time.Now()
+	_, err := syscall.Write(w.fd, bytes)
+	if err != nil {
+		glog.Fatal(err)
+	}
+	if w.mode == "fsync" || w.mode == "direct" {
+		err = syscall.Fsync(w.fd)
+		if err != nil {
+			glog.Fatal(err)
+		}
+	}
+	if time.Since(startTime) > time.Millisecond {
+		glog.Info(" bytes written & synced to persistent log in ", time.Since(startTime).String())
+	}
+	glog.V(1).Info(" bytes written & synced to persistent log in ", time.Since(startTime).String())
+}
