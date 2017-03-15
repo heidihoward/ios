04df40ac61802fdc16655b01ffa145bceb580862
heidi-ann <hh360@cam.ac.uk>
2017-03-15 17:29:34 +0000

    trying to add locking to window, NOT WORKING ATM

--- consensus/window.go
+++ consensus/window.go
@@ -1,10 +1,16 @@
 package consensus
 
+import (
+	"sync"
+	"github.com/golang/glog"
+)
+
 type rwindow struct {
 	outstanding map[int]bool // outstanding holds in progress request indexes
 	ready       chan int     // indexes that can be allocated
 	windowStart int          // the last committed entry, window is from windowStart+1
 	windowSize  int          // limit on the size of the window, window is to windowStart+windowSize
+	sync.RWMutex // lock for concurrent access to outstanding
 }
 
 func newReplicationWindow(startIndex int, windowSize int) *rwindow {
@@ -12,23 +18,36 @@ func newReplicationWindow(startIndex int, windowSize int) *rwindow {
 	ready := make(chan int, windowSize)
 	for i := startIndex + 1; i <= startIndex+windowSize; i++ {
 		ready <- i
+		outstanding[i] = false
 	}
-	return &rwindow{outstanding, ready, startIndex, windowSize}
+	return &rwindow{outstanding, ready, startIndex, windowSize,sync.RWMutex{}}
 }
 
 func (rw *rwindow) nextIndex() int {
 	index := <-rw.ready
+	rw.Lock()
 	rw.outstanding[index] = true
+	rw.Unlock()
+	glog.V(1).Info("Allocating index ",index)
 	return index
 }
 
 func (rw *rwindow) indexCompleted(index int) {
 	// remove from outstanding
+	rw.Lock()
 	delete(rw.outstanding, index)
+	rw.Unlock()
+	glog.Info("Index ",index," is completed")
 	// check if we can advance the windowStart
 	// if so, indexes can be loaded into ready
+	rw.Lock()
 	for _, ok := rw.outstanding[rw.windowStart+1]; !ok; {
 		rw.windowStart++
-		rw.ready <- rw.windowStart + rw.windowSize
+		newIndex := rw.windowStart + rw.windowSize
+		glog.Info("Advancing window to ",rw.windowStart)
+		glog.Info("Adding index ",newIndex)
+
+		rw.outstanding[newIndex] = false
 	}
+	rw.Unlock()
 }
--- consensus/window_test.go
+++ consensus/window_test.go
@@ -2,12 +2,20 @@ package consensus
 
 import (
 	"testing"
+	"github.com/golang/glog"
 )
 
 func TestNextIndex(t *testing.T) {
+	glog.Info("Starting window test 0")
 	window := newReplicationWindow(-1, 1)
 	index := window.nextIndex()
 	if index != 0 {
+		t.Error("ReplicationWindow giving wrong index")
+	}
+	window.indexCompleted(index)
+	glog.Info("Starting window test 1")
+	index2 := window.nextIndex()
+	if index2 != 1 {
 		t.Error("ReplicationWindow giving wrong index")
 	}
 }
