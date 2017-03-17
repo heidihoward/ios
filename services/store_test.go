205446ad45bc3d8ac5c43bf35a336c0872886fe7
hesthorp <hesthorp@gmail.com>
2017-03-17 20:59:53 +0000

    Add marshalling tests

--- services/store_test.go
+++ services/store_test.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"strconv"
 	"testing"
 
 	"fmt"
@@ -60,4 +61,36 @@ func TestCheckFormat(t *testing.T) {
 	assert.True(t, dummy.CheckFormat("ping"))
 	assert.False(t, dummy.CheckFormat(""))
 	assert.False(t, dummy.CheckFormat("update A 1"))
+}
+
+func TestMarshalling(t *testing.T) {
+	store := StartService("kv-store")
+	// add data to store
+	store.Process("update A 1")
+	for i := 0; i < 100; i++ {
+		store.Process(fmt.Sprintf("update %v %v", i, i))
+	}
+	store.Process("delete 0")
+
+	//marshal store
+	json, err := store.MarshalJSON()
+	if err != nil {
+		t.Fatal("err marshalling store. Error: ", err.Error())
+	}
+	//make more changes (should be overwritten when unmarshalling)
+	store.Process("delete 1")
+
+	//unmarshal and validate store
+	err = store.UnmarshalJSON(json)
+	if err != nil {
+		t.Fatal("err unmarshalling store. Error: ", err.Error())
+	}
+
+	assert.Equal(t, "1", store.Process("get A"))
+	assert.Equal(t, "false", store.Process("exists 0"))
+	assert.Equal(t, "100", store.Process("count"))
+	for i := 1; i < 100; i++ {
+		assert.Equal(t, strconv.Itoa(i), store.Process(fmt.Sprintf("get %v", i)))
+	}
+
 }
