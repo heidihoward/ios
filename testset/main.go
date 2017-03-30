9c1be0043d727fb7b7fc12c048b121605a9c8ebe
Heidi-ann <hh360@cam.ac.uk>
2017-03-30 17:13:01 +0100

    finishing client set tester

--- testset/main.go
+++ testset/main.go
@@ -7,19 +7,18 @@ import (
 	"github.com/heidi-ann/ios/config"
 	"github.com/heidi-ann/ios/test/generator"
 	"os"
-	"os/signal"
-	"syscall"
+	"strconv"
 	"time"
 )
 
 var configFile = flag.String("config", os.Getenv("GOPATH")+"/src/github.com/heidi-ann/ios/client/example.conf", "Client configuration file")
 var autoFile = flag.String("auto", os.Getenv("GOPATH")+"/src/github.com/heidi-ann/ios/test/workload.conf", "Configure file for workload")
-var number = flag.Int("n",1, "Number of clients to create")
+var clients = flag.Int("clients",1, "Number of clients to create")
 
 // runClient returns when workload is finished or SubmitRequest fails
-func runClient(id int ,addresses conf.Addresses.Address, timeout time.Duration, workloadConfig config.WorkloadConfig) {
-	c := client.StartClient(id, "latency_"+id+".csv", addresses, timeout)
-	ioapi := generator.Generate(config.ParseWorkloadConfig(*autoFile), *consistencyCheck)
+func runClient(id int, addresses []string, timeout time.Duration, workloadConfig config.WorkloadConfig) {
+	c := client.StartClient(-1, "latency_"+strconv.Itoa(id)+".csv", addresses, timeout)
+	ioapi := generator.Generate(workloadConfig, false)
 
 	for {
 		// get next command
@@ -43,48 +42,26 @@ func main() {
 	flag.Parse()
 	defer glog.Flush()
 
-	// always flush (whatever happens)
-	sigs := make(chan os.Signal, 1)
-	finish := make(chan bool, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
 	// parse config files
+	finished := make(chan bool)
 	conf := config.ParseClientConfig(*configFile)
 	timeout := time.Millisecond * time.Duration(conf.Parameters.Timeout)
-	workloadConf := config.ParseWorkloadConfig(*autoFile)
+	workloadConfig := config.ParseWorkloadConfig(*autoFile)
 
-	c := client.StartClient(*id, *statFile, conf.Addresses.Address, timeout)
-
-	// setup API
-	ioapi := generator.Generate(config.ParseWorkloadConfig(*autoFile), *consistencyCheck)
-
-	go func() {
-		for {
-			// get next command
-			text, read, ok := ioapi.Next()
-			if !ok {
-				finish <- true
-				break
+  remaining := *clients
+	for id := 0; id < *clients; id++ {
+		go func(id int) {
+			runClient(id, conf.Addresses.Address, timeout, workloadConfig)
+			remaining--
+			if remaining == 0 {
+				finished <- true
 			}
-			// pass to ios client
-			success, reply := c.SubmitRequest(text, read)
-			if !success {
-				finish <- true
-				break
-			}
-			// notify API of result
-			ioapi.Return(reply)
-
-		}
-	}()
-
-	select {
-	case sig := <-sigs:
-		glog.Warning("Termination due to: ", sig)
-	case <-finish:
-		glog.Info("No more commands")
+		}(id)
 	}
-	c.StopClient()
+
+	// wait for workload to finish
+	<- finished
+	glog.Info("Client set terminating")
 	glog.Flush()
 
 }
